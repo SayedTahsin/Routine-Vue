c3047264901a35424b1a28a18cee83ae4fe98a1a
SayedTahsin <mail.tahsin99@gmail.com>
2024-11-05 21:02:54 +0600

    login working
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- src/components/loginView.vue
+++ src/components/loginView.vue
@@ -1,41 +1,40 @@
 <template>
-    <div class="login-component">
-        <button @click="signInWithGoogle">Sign in with Google</button>
+    <div class="login-component flex items-center justify-center h-screen">
+        <button @click="signInWithGoogle"
+            class="flex items-center px-4 py-2 space-x-2 border border-gray-300 rounded-md shadow-sm hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-600">
+            <img src="/google.png" alt="Google icon" class="w-5 h-5" />
+            <span class="text-gray-700 dark:text-gray-200 font-medium">
+                Sign in with Google
+            </span>
+        </button>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import axios from '../plugins/axios';
 import { useUserStore } from '@/stores/user';
 import { auth, provider } from '@/firebase';
 import { signInWithPopup } from 'firebase/auth';
 
 const userStore = useUserStore();
-const error = ref(null);
 
 async function signInWithGoogle() {
     try {
-        // Sign in with Google
         const result = await signInWithPopup(auth, provider);
         const googleUser = result.user;
         const userPayload = { mail: googleUser.email || '', id: googleUser.uid || '', photoUrl: googleUser.photoURL || '', name: googleUser.displayName || '' };
 
-        // // Call API to save user data if first time login
-        await axios.post('api/users', userPayload);
+        await axios.post('api/users', userPayload, {
+            withCredentials: true
+        });
 
-        // // Call login API to set cookie
-        // await axios.post('http://localhost:3000/login', userPayload, {
-        //     withCredentials: true, // Allow cookies to be sent with request
-        // });
+        await axios.post('auth/login', userPayload, {
+            withCredentials: true,
+        });
 
-        console.log(googleUser)
-
-        // Update Pinia store with user data
         userStore.setUser(userPayload);
     } catch (err: unknown) {
-        error.value = err.message;
-        console.error('Login error:', error.value);
+        console.error('Login error:', err);
     }
 }
 </script>
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'https://routine.mail-tahsin99.workers.dev/',
+  baseURL: 'http://localhost:8787/',
   timeout: 1000,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 })
+// https://routine.mail-tahsin99.workers.dev/
 
 export default axiosInstance
