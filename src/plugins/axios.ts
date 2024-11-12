0dd6a0d01a40629367a772ad807ba10d396e3ba4
SayedTahsin <mail.tahsin99@gmail.com>
2024-11-12 23:56:57 +0600

    Axios fix
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8787/',
+  baseURL: import.meta.env.VITE_API_URL, //http://localhost:8787/
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 })
-// import.meta.env.VITE_API_URL
+
 export default axiosInstance
